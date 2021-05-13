8e3be4d8808f20951e5409c79ecc66e348864117
xiaoxiao650 <76093417+xiaoxiao650@users.noreply.github.com>
2021-05-13 16:03:53 +0800

    Add files via upload

---
+++ guanyugonglue/js/common.js
@@ -0,0 +1,5 @@
+ window.onload = window.onresize = function(){
+	        var view_width = document.getElementsByTagName('html')[0].getBoundingClientRect().width;
+	        var _html = document.getElementsByTagName('html')[0];
+	        view_width>640?_html.style.fontSize = 640/16 + 'px':_html.style.fontSize = view_width / 16 + 'px';
+	    }
---
+++ guanyugonglue/js/nav.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('#nav_show').click(function(){
+        $('#nav').css('display','block');
+    });
+    $('#nav_none').click(function(){
+        $('#nav').css('display','none');
+    });
+})
